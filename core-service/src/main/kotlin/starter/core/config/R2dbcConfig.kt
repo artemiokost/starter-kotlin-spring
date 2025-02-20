69d3600caf99bf3809378926e0c7a5b3205c1a51
artemiokost <artemiokost@gmail.com>
2025-02-20 19:55:15 +0700

    starter -- tx manager config added

--- core-service/src/main/kotlin/starter/core/config/R2dbcConfig.kt
+++ core-service/src/main/kotlin/starter/core/config/R2dbcConfig.kt
@@ -1,15 +1,19 @@
 package starter.core.config
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 import org.springframework.data.r2dbc.dialect.PostgresDialect
+import org.springframework.r2dbc.connection.R2dbcTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import starter.core.converter.JsonToMapAnyConverter
 
 @Configuration
 @EnableR2dbcAuditing
+@EnableTransactionManagement
 class R2dbcConfig {
 
     @Bean
@@ -17,5 +21,10 @@ class R2dbcConfig {
         val converters: MutableList<Converter<*, *>> = ArrayList()
         converters.add(JsonToMapAnyConverter())
         return R2dbcCustomConversions.of(PostgresDialect.INSTANCE, converters)
+    }
+
+    @Bean
+    fun r2dbcTransactionManager(connectionFactory: ConnectionFactory): R2dbcTransactionManager {
+        return R2dbcTransactionManager(connectionFactory)
     }
 }
